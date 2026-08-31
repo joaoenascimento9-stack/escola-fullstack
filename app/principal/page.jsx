fa3f6d14786b9f2aeb16b3eeae091d172550c73f
Emanuel <joao.e.nascimento9@aluno.senai.br>
2026-08-31 12:44:53 -0300

    Atualização do Projeto

---
+++ app/principal/page.jsx
@@ -0,0 +1,14 @@
+"use client"
+import Header from "../components/header";
+
+export default function Principal() {
+    return (
+        <>
+            <Header />
+            <main>
+                <h2>Bem Vindo ao Sistema Escolar - Sesi Mirandópolis</h2>
+                <p>Onde você pode acompanhar suas notas e faltas!</p>
+            </main>
+        </>
+    );
+}
